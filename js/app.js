c39d763dc8b91e4853e38c5fec587abae19aaa58
William Weisberger <bill.weisberger@gmail.com>
2019-06-24 20:05:54 -0600

    crazy object

--- js/app.js
+++ js/app.js
@@ -30,9 +30,19 @@ console.log('window is loaded');
 //
 // 2. Take it Easy
 // Make an array that holds all of the colors of the rainbow.
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 // Write code that will access "blue" from the rainbow array.
+console.log(colors[4]);
 // Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+const bill = {
+  favoriteFood: 'hot dogs',
+  hobby: 'building model ships',
+  currentLocation: 'Denver',
+  favoriteDataType: 'arrays'
+}
 // Write code that will access your hobby from the object that you just created.
+console.log(bill.hobby);
+console.log(bill["hobby"]);
 // 🔴 Commit: "data structures"
 //
 // 3. Crazy Object!
@@ -62,18 +72,23 @@ const crazyObject = {
       {
         name: "susie",
         occupation: "jeffs wife",
-        favourtieHobby: "Swearing at Larry and Jeff"
+        favouriteHobby: "Swearing at Larry and Jeff"
       },
     ]
   }
 }
 // Use crazyObject to log the following.
-//
+
 // "omg my mouth is burning"
+console.log(crazyObject.taco[1].salsa[5]);
 // "Pretty pretty prettayyyyy good"
+console.log(crazyObject.larry.quotes[0]);
 // "Swearing at Larry and Jeff"
+console.log(crazyObject.larry.characters[2].favouriteHobby);
 // "Chicken Teriyaki Boyyyyyy"
+console.log(crazyObject.larry.nicknames[1]);
 // The object the contains the name funkhauser
+console.log(crazyObject.larry.characters[1]);
 // 🔴 Commit: "crazyObject"
 //
 // 4. Object-ception
@@ -145,12 +160,12 @@ const bondFilms = [
 // Console log the single movie object that contains the actor who starred in the least number of films.
 // Expected result:
 
-{
-  "title" : "On Her Majesty's Secret Service",
-  "year" : 1969,
-  "actor" : "George Lazenby",
-  "gross" : "$505,899,782"
-}
+// {
+//   "title" : "On Her Majesty's Secret Service",
+//   "year" : 1969,
+//   "actor" : "George Lazenby",
+//   "gross" : "$505,899,782"
+// }
 // hint: Objects by definition have unique keys. Later in the problem you could create a new object wherein all the Bond actors are keys, and unique, with no doubles. hint within a hint: If you need to iterate over an object, remember you can use Object.keys() or a for ... in loop
 
 // 🔴 Commit "humdinger"
